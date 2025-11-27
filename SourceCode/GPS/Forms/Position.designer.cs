4c4801d26c0a09dd4310d4e7b044cd0ec6026d3b
BrianTee <yesgogit@gmail.com>
2025-11-26 18:16:03 -0700

    Fix sectionTrigger Step distance on curves not being too long.

--- SourceCode/GPS/Forms/Position.designer.cs
+++ SourceCode/GPS/Forms/Position.designer.cs
@@ -1395,21 +1395,21 @@ namespace AgOpenGPS
             //used to increase triangle countExit when going around corners, less on straight
             //pick the slow moving side edge of tool
             double distance = tool.width*0.75;
-            if (distance > 8) distance = 8;
+            if (distance > 5) distance = 5;
 
             //whichever is less
             if (tool.farLeftSpeed < tool.farRightSpeed)
             {
                 double twist = tool.farLeftSpeed * (tool.width / 50) / tool.farRightSpeed * (50/ tool.width);
                 twist *= twist;
-                if (twist < 0.2) twist = 0.2;
+                if (twist < 0.1) twist = 0.1;
                 sectionTriggerStepDistance = distance * twist;
             }
             else
             {
                 double twist = tool.farRightSpeed * (tool.width / 50) / tool.farLeftSpeed * (50 / tool.width);
                 twist *= twist;
-                if (twist < 0.2) twist = 0.2;
+                if (twist < 0.1) twist = 0.1;
 
                 sectionTriggerStepDistance = distance * twist;
             }
